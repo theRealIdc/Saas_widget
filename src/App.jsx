5117061b4d6a8154f5e50f5bd98a8a531e186215
christIkongaPG <christ@peruzzo-group.com>
2024-08-10 18:35:53 +0200

    feat: start project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="fixed bottom-4 z-50 right-4">
+      <Button className="rounded-full shadow-lg hover:scale-105">
+        FeedBack
+      </Button>
+    </div>
+  );
+}
+
+export default App;
